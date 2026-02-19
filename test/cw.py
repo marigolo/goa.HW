7980a13c4b4319d9089b60a23c69df3818bfda58
marigolo <marigoloshvili6@gmail.com>
2026-02-19 18:21:45 +0400

    hw 5

---
+++ test/cw.py
@@ -0,0 +1,30 @@
+
+
+
+
+#debbugging არის კოდში errorის გასწორება თუ კოდში ყველა error-ს არ გავასწორებთ კოდი არ გაეშვება
+
+name = "giorgi"
+print(name)
+
+
+num = 5
+text = "apple"
+result = str(num) + text
+print(result)
+
+
+#3
+
+city = input("enter your city")
+district = input("enter your district")
+buillding = input("enter your buillding number")
+print(f"your city is{city} your district is {district} your bilding number is {buillding}")
+#4) print არის ბრძანება რომელსაც ტექსტი გამოაქვს ტერმინალში
+
+
+#5)
+name = "Mari"
+last_name = "goloshvili"
+hobby = "playing volleyball"
+print(f"{name} {last_name} {hobby}")
