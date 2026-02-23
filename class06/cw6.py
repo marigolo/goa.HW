22e3cbd174a1f5e42c58736544d6d27a9cb4d4d8
marigolo <marigoloshvili6@gmail.com>
2026-02-23 20:25:36 +0400

    cw6abgrade

--- class06/cw6.py
+++ class06/cw6.py
@@ -8,9 +8,11 @@ print(num1 // num2)
 print(num1 ** num2)
 print(num1 % num2)
 
+#2) გამოიტანს True რადგან True არის 1 და False არის 0 ამიტომ 1-0+0=1 ბოლოს ვიღებთ 1-ს და არის True!
+print(bool(int(True) - int(False) + int(False)))
 
-
-
-#2) გამოიტანს True რადგან True არის 1 და False არის 0 ამიტომ 1-0+0 =1ბოლოს ვიღებთ 1-ს
-
-#3) int()-გადაყავს str int  სტრ() გადაყავს ნებისმიერი სტრინგად flote()გადაყავს ფლოუტად bool() gadayavs boolianad
+#3)
+#  int()   - გადაყავს int;
+#  str()   - გადაყავს ნებისმიერი string;
+#  flote() - გადაყავს flote;
+#  bool()  - გადაყავს bullian;
